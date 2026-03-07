61acfdd584517689d73d77f3b1cd8a73ce162438
aga-im <agafendii@gmail.com>
2026-03-07 12:43:03 +0300

    add a few tests

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -1,39 +1,68 @@
 package app
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/aga-absolut/LoyaltyProgram/internal/config"
 	"github.com/aga-absolut/LoyaltyProgram/middleware/logger"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestApp_RegisterHandler(t *testing.T) {
-	cfg := &config.Config{}
-	log := &logger.Logger{}
+	cfg := &config.Config{DatabaseDSN: "postgres://postgres:absolute_1@localhost:5432/LoyalProgram"}
+	log := logger.NewLogger()
+	storage := NewStorage(cfg, log)
 	ch := make(chan string)
-	app := NewApp(cfg, log, testStorage, ch)
-	type Credentials struct {
-		Login    string `json:"login"`
-		Password string `json:"password"`
-	}
+	app := NewApp(cfg, log, storage, ch)
 	tests := []struct {
 		name        string
 		statusCode  int
 		contentType string
-		Credentials Credentials
+		body        string
 	}{
-		{
-			name:        "first test",
-			statusCode:  200,
-			contentType: "application/json",
-			Credentials: Credentials{
-				Login:    "absolute",
-				Password: "123456",
-			},
-		},
+		{"first test", http.StatusOK, "application/json", `{"login":"absolute1", "password":"12345678"}`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodPost, "/api/user/register", strings.NewReader(tt.body))
+			w := httptest.NewRecorder()
+			app.RegisterHandler(w, r)
+			res := w.Result()
+
+			assert.Equal(t, tt.contentType, res.Header.Get("Content-Type"))
+			assert.Equal(t, tt.statusCode, res.StatusCode)
+		})
+	}
+}
+
+func TestApp_AuthHandler(t *testing.T) {
+	cfg := &config.Config{DatabaseDSN: "postgres://postgres:absolute_1@localhost:5432/LoyalProgram"}
+	log := logger.NewLogger()
+	storage := NewStorage(cfg, log)
+	ch := make(chan string)
+	app := NewApp(cfg, log, storage, ch)
+
+	tests := []struct {
+		name        string
+		statusCode  int
+		contentType string
+		body        string
+	}{
+		{"first test", http.StatusUnauthorized, "text/plain; charset=utf-8", `{"login":"absolute123", "password":"12345678"}`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodPost, "/api/user/orders", strings.NewReader(tt.body))
+			w := httptest.NewRecorder()
+
+			app.AuthHandler(w, r)
+			res := w.Result()
+
+			assert.Equal(t, tt.contentType, res.Header.Get("Content-Type"))
+			assert.Equal(t, tt.statusCode, res.StatusCode)
 		})
 	}
 }
