9ce4efd883996153d7256368aca787d8d92c4707
aga-im <agafendii@gmail.com>
2026-03-07 13:00:43 +0300

    final version

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -23,14 +23,16 @@ func TestApp_RegisterHandler(t *testing.T) {
 		contentType string
 		body        string
 	}{
-		{"first test", http.StatusOK, "application/json", `{"login":"absolute1", "password":"12345678"}`},
+		{"first test", http.StatusOK, "application/json", `{"login":"absolute123", "password":"12345678"}`},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := httptest.NewRequest(http.MethodPost, "/api/user/register", strings.NewReader(tt.body))
 			w := httptest.NewRecorder()
 			app.RegisterHandler(w, r)
+
 			res := w.Result()
+			defer res.Body.Close()
 
 			assert.Equal(t, tt.contentType, res.Header.Get("Content-Type"))
 			assert.Equal(t, tt.statusCode, res.StatusCode)
@@ -55,11 +57,12 @@ func TestApp_AuthHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodPost, "/api/user/orders", strings.NewReader(tt.body))
+			r := httptest.NewRequest(http.MethodPost, "/api/user/login", strings.NewReader(tt.body))
 			w := httptest.NewRecorder()
-
 			app.AuthHandler(w, r)
+
 			res := w.Result()
+			defer res.Body.Close()
 
 			assert.Equal(t, tt.contentType, res.Header.Get("Content-Type"))
 			assert.Equal(t, tt.statusCode, res.StatusCode)
